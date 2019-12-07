69d9d648dccd2d888468e05ee3f13170fcd85684
sandipgole <golesandip98@gmail.com>
2019-12-07 16:19:38 +0545

    AcceptingClientData

---
+++ 04_AcceptingClientData/src/main/java/com/example/demo/DataController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DataController {
+	@RequestMapping("Home")
+	public String Home(HttpServletRequest req)
+	{ HttpSession Session=req.getSession();
+	String name=req.getParameter("name");
+	Session.setAttribute("name",name);
+		return "Home.jsp";
+	}
+
+}
+ 
